5d33cbf18066b323959cb82fc232f44b8eba1231
Mathias <mato058@student.kristiania.no>
2024-05-28 10:25:17 +0200

    Added empty backend
    Empty spring boot project

---
+++ Server/src/main/java/loading/smidig/smidig/SmidigApplication.java
@@ -0,0 +1,13 @@
+package loading.smidig.smidig;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmidigApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmidigApplication.class, args);
+	}
+
+}
---
+++ Server/src/test/java/loading/smidig/smidig/SmidigApplicationTests.java
@@ -0,0 +1,13 @@
+package loading.smidig.smidig;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmidigApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
