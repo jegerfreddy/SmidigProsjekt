4ce3c5f913b791a16873dbeaa22857f9be8b20c8
Mathias <mato058@student.kristiania.no>
2024-05-28 14:36:05 +0200

    Backend - Persist data

--- Server/src/main/java/loading/smidig/smidig/controller/UserController.java
+++ Server/src/main/java/loading/smidig/smidig/controller/UserController.java
@@ -1,8 +1,9 @@
 package loading.smidig.smidig.controller;
 
 import loading.smidig.smidig.model.User;
-import loading.smidig.smidig.repository.UserRepository;
 import loading.smidig.smidig.service.UserService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -13,6 +14,9 @@ import java.util.List;
 public class UserController {
 
     private final UserService userService;
+
+    //Logger
+    private static final Logger log = LoggerFactory.getLogger(UserController.class);
 
     @Autowired
     public UserController(UserService userService) {
@@ -34,18 +38,21 @@ public class UserController {
     //Create user
     @PostMapping("/new")
     public User createUser(@RequestBody User user) {
+        log.info("User created: " + user.getUsername());
         return userService.createUser(user);
     }
 
     //Update user
     @PutMapping("/update")
     public User updateUser(@RequestBody User user) {
+        log.info("User updated: " + user.getUsername());
         return userService.createUser(user);
     }
 
     //Delete user by ID
     @DeleteMapping("/delete/{id}")
     public void deleteUser(@PathVariable Long id) {
+        log.info("User deleted: " + userService.getUserById(id).getUsername());
         userService.deleteUser(id);
     }
 }
