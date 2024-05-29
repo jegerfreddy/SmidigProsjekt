9a1733d771b16d67db9feca0f3fecfc0db07b917
eliasWolden <146863240+eliasWolden@users.noreply.github.com>
2024-05-29 18:49:45 +0200

    chore: Update Routing component with context providers

--- Frontend/user/src/Routes/Routing.tsx
+++ Frontend/user/src/Routes/Routing.tsx
@@ -11,23 +11,29 @@ import GameLobby from '../Pages/GameLobby';
 import PhoneInfo from '../assets/Phone/PhoneInfo';
 import TheaterPausePage from '../Pages/TheaterPausePage'
 import WaitingPage from '../Pages/WaitingPage';
+import { GeneralProvider } from '../Contexts/UserContext';
+import { ActService, UserService } from '../Services/GetService';
 
 const Routing: React.FC = () => {
     PhoneInfo();
     return (
-        <Router>
-            <Routes>
-                <Route path="/" element={<UserLoginPage />} />
-                <Route path="/login" element={<StartPage />} />
-                <Route path="/username" element={<ChooseUserNamePage />} />
-                <Route path="/avatar" element={<ChooseAvatarPage />} />
-                <Route path="/gameLobby" element={<GameLobby />} />
-                <Route path="/voting" element={<VotingPage />} />
-                <Route path="/result" element={<ResultPage />} />
-                <Route path="/Break" element={<TheaterPausePage/>} />
-                <Route path="/Waiting" element={<WaitingPage/>} />
-            </Routes>
-        </Router>
+    <GeneralProvider service={ActService}>
+        <GeneralProvider service={UserService}>
+                <Router>
+                    <Routes>
+                        <Route path="/" element={<UserLoginPage />} />
+                        <Route path="/login" element={<StartPage />} />
+                        <Route path="/username" element={<ChooseUserNamePage />} />
+                        <Route path="/avatar" element={<ChooseAvatarPage />} />
+                        <Route path="/gameLobby" element={<GameLobby />} />
+                        <Route path="/voting" element={<VotingPage />} />
+                        <Route path="/result" element={<ResultPage />} />
+                        <Route path="/Break" element={<TheaterPausePage/>} />
+                        <Route path="/Waiting" element={<WaitingPage/>} />
+                    </Routes>
+                </Router>
+        </GeneralProvider>
+    </GeneralProvider>
     );
 };
 
