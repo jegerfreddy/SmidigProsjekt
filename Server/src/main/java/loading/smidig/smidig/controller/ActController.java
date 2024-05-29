b35f4de1d5ae031b76fb180183289f8cfb38e042
Mathias <mato058@student.kristiania.no>
2024-05-29 12:44:50 +0200

    Update ActController.java

--- Server/src/main/java/loading/smidig/smidig/controller/ActController.java
+++ Server/src/main/java/loading/smidig/smidig/controller/ActController.java
@@ -9,6 +9,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api/act")
+@CrossOrigin(origins = "http://localhost:5173")
 public class ActController {
 
     private final ActService actService;
