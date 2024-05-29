5fd71ac01eaf194b696de6bf792167c8dd5198ce
parmdeepsinghh <parmdeep.singhh@gmail.com>
2024-05-29 14:44:48 +0200

    Logo. Css.. mediaqueries
    La inn midlertidig logo. Endra css på den for å funke når telefonen snus. brukt mediaqueries

--- Frontend/user/src/Pages/TheaterPausePage.tsx
+++ Frontend/user/src/Pages/TheaterPausePage.tsx
@@ -1,13 +1,14 @@
 import React from "react";
-import{ ResumeBtn } from "../assets/Button/ResumeBtn";
+import { ResumeBtn } from "../assets/Button/ResumeBtn";
 
 const TheaterPausePage: React.FC = () => {
     return (
-        <main className="min-vh-100 ">
+        <main className="min-vh-100 d-flex flex-column">
             <div className="text-center">
                 <h1>Game Paused</h1>
-                <ResumeBtn/>
+                <img src="/images/loading_logo.png" alt="Loading" className="img-fluid" />
             </div>
+            <ResumeBtn />
         </main>
     );
 };
