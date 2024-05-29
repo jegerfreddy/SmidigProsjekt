bee68a822e7ebea49a54d58f5f5bc3881dd7886d
eliasWolden <146863240+eliasWolden@users.noreply.github.com>
2024-05-29 13:04:54 +0200

    chore: Update avatar styling and add avatar list to ChooseAvatarPage voting page

--- Frontend/user/src/Pages/VotingPage.tsx
+++ Frontend/user/src/Pages/VotingPage.tsx
@@ -1,16 +1,20 @@
 import React from 'react';
 import { NextBtn } from '../assets/Button/NextBtn';
+import VotingList from '../assets/Voting/VotingList';
 
 const VotingPage: React.FC = () => {
     return (
-        <main className='min-vh-100'>
-            <h1>Voting Page</h1>
-
-            <div>
-                
+        <main className='position-relative vh-100'>
+            <div className='position-absolute top-0 start-50 translate-middle-x mt-5'>
+                <img src="images/Loading.png" alt="Loading" />
             </div>
-        
-            <NextBtn path='result' />
+
+            <div className='position-absolute top-50 start-50 translate-middle'>
+                <VotingList />
+            </div>
+            <div className=''>
+                <NextBtn path='result' />
+            </div>
         </main>
     );
 };
