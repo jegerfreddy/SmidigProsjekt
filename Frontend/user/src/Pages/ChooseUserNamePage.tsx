f1b665cfdec615fa4b09f4aded87b80c63fff074
parmdeepsinghh <parmdeep.singhh@gmail.com>
2024-05-30 15:01:22 +0200

    Knapp justert i brukernavn page

--- Frontend/user/src/Pages/ChooseUserNamePage.tsx
+++ Frontend/user/src/Pages/ChooseUserNamePage.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 const ChooseUserNamePage: React.FC = () => {
     const [username, setUsername] = React.useState('');
@@ -10,21 +11,27 @@ const ChooseUserNamePage: React.FC = () => {
     };
 
     return (
-        <main className='min-vh-100'>
-            <h1 className='text-center'>Registrering</h1>
-            <input 
-                type="text" 
-                placeholder='Brukernavn'
-                className='position-absolute bottom-50 start-50 translate-middle m-3 userNameInput'
-                value={username}
-                onChange={(e) => setUsername(e.target.value)}
-            />
-            <div className='position-absolute bottom-0 start-50 translate-middle m-3'>
+        <div className='position-relative vh-100'>
+            <h1 className='position-absolute top-0 start-50 translate-middle-x mt-4'>
+                Registrering
+            </h1>
+
+            <div className='position-absolute top-50 start-50 translate-middle'>
+                <input 
+                    type="text" 
+                    placeholder='Brukernavn'
+                    className='userNameInput'
+                    value={username}
+                    onChange={(e) => setUsername(e.target.value)}
+                />
+            </div>
+
+            <div className='position-absolute bottom-0 start-50 translate-middle-x mb-4'>
                 <button className='pinButton' onClick={createUserName}>
                     Fortsett
                 </button>
             </div>
-        </main>
+        </div>
     );
 };
 
