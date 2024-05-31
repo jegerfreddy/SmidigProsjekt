5158ada102cbfdccfd161f60d7a110d62cb5a047
parmdeepsinghh <parmdeep.singhh@gmail.com>
2024-05-31 11:28:19 +0200

    oppdatert knapp

--- Frontend/user/src/assets/Button/ResumeBtn.tsx
+++ Frontend/user/src/assets/Button/ResumeBtn.tsx
@@ -1,9 +1,11 @@
 import React from "react";
+import { NextBtnProps } from "../../Interfaces/IButton";
 
-export const ResumeBtn: React.FC = () => {
+
+export const ResumeBtn: React.FC<NextBtnProps> = ({ path }) => {
     return (
         <div className='d-flex justify-content-center align-items-center position-absolute bottom-0 start-50 translate-middle-x mb-3'>
-            <button className="pinButton">
+            <button className="pinButton" onClick={() => window.location.href = `/${path}`}>
                 Resume
             </button>
         </div>
