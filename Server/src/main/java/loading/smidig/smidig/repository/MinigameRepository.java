d50369bd6f93bf77dd7a1eb7f0245f2dbc001400
Mathias <mato058@student.kristiania.no>
2024-06-03 13:42:25 +0200

    Server - Backend; Minigames & results table

---
+++ Server/src/main/java/loading/smidig/smidig/repository/MinigameRepository.java
@@ -0,0 +1,8 @@
+package loading.smidig.smidig.repository;
+
+
+import loading.smidig.smidig.model.Minigame;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface MinigameRepository extends JpaRepository<Minigame, Long> {
+}
---
+++ Server/src/main/java/loading/smidig/smidig/repository/MinigameResultRepository.java
@@ -0,0 +1,7 @@
+package loading.smidig.smidig.repository;
+
+import loading.smidig.smidig.model.MinigameResult;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface MinigameResultRepository extends JpaRepository<MinigameResult, Long> {
+}
