c01126356367f66306ad16721941ef34a73e774d
parmdeepsinghh <parmdeep.singhh@gmail.com>
2024-06-04 12:10:11 +0200

    Update Routing.tsx
    La EndGamePage inn i Route. Ikke noe som peker mot den enda

--- Frontend/user/src/Routes/Routing.tsx
+++ Frontend/user/src/Routes/Routing.tsx
@@ -16,6 +16,7 @@ import WaitResultPage from '../Pages/VotingResultPages/WaitResultPage';
 import GameLobby from '../Pages/WaitingPages/GameLobby';
 import StandByPage from '../Pages/WaitingPages/StandByPage';
 import WaitingLobbyPage from '../Pages/WaitingPages/WaitingLobbyPage';
+import EndGamePage from '../Pages/WaitingPages/EndGamePage';
 
 
 const Routing: React.FC = () => {
@@ -59,7 +60,8 @@ const Routing: React.FC = () => {
 
                 <Route path="/Break" element={<TheaterPausePage />} />
                 <Route path="/Waiting" element={<WaitingLobbyPage />} />
-
+                <Route path="/EndGame" element={<EndGamePage/>} />
+                
                 <Route path="/tie/:actEventId" element={<TiePage />} />
                 <Route path="/minigame/:actEventId" element={<MiniGamePage />} />
                 <Route path="/feedBackPage" element={<FeedBackPage />} />
