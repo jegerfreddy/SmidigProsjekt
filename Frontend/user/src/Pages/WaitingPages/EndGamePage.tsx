0c40df77b5d8e816efd60a20d9dacc210c91f63d
parmdeepsinghh <parmdeep.singhh@gmail.com>
2024-06-04 12:09:40 +0200

    Lagde EndGamePage
    Side som skal henvise videre til feedback. Typisk GameCredits. Kan flyttes etter feedbackPage

---
+++ Frontend/user/src/Pages/WaitingPages/EndGamePage.tsx
@@ -0,0 +1,16 @@
+import React, { useState, useEffect } from "react";
+
+const EndGamePage: React.FC = () => {
+   return(
+    <main className="min-vh-100 bgColor">
+        <div className="text-center fs-1">
+            Game Over
+        </div>
+        <div>
+            Halla
+        </div>
+    </main>
+   ) 
+};
+
+export default EndGamePage
