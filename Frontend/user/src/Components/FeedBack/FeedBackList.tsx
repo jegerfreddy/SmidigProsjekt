ae846642a6ac912a9afa1415d53ed68c72114d32
eliasWolden <146863240+eliasWolden@users.noreply.github.com>
2024-06-04 15:12:09 +0200

    Malin sin feedback page

---
+++ Frontend/user/src/Components/FeedBack/FeedBackList.tsx
@@ -0,0 +1,20 @@
+import {useState } from "react";
+import FeedBackItem from "./FeedBackItem";
+import { FeedBackListProps } from "../../Interfaces/IFeedBack";
+
+
+
+
+const FeedBackList: React.FC<FeedBackListProps> = ({onClick }) => {
+    const [error] = useState<string | null>(null);
+
+
+    return (
+        <div className="container">
+            <FeedBackItem onClick={onClick}/>
+            {error && <div>{error}</div>}
+        </div>
+    );
+}
+
+    export default FeedBackList;
