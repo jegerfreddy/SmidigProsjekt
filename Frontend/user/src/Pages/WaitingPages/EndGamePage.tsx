135635af13e9730d3cd4a92b3ec69e6f7db9b612
parmdeepsinghh <parmdeep.singhh@gmail.com>
2024-06-04 12:36:01 +0200

    Update EndGamePage.tsx
    Endringer på EndGamePage

--- Frontend/user/src/Pages/WaitingPages/EndGamePage.tsx
+++ Frontend/user/src/Pages/WaitingPages/EndGamePage.tsx
@@ -3,11 +3,11 @@ import React, { useState, useEffect } from "react";
 const EndGamePage: React.FC = () => {
    return(
     <main className="min-vh-100 bgColor">
-        <div className="text-center fs-1">
-            Game Over
+        <div className="text-center">
+            <img src="/images/LoadingLogo.png" alt="Loading" className="" />    
         </div>
-        <div>
-            Halla
+        <div className="text-center fs-1 z-index-3">
+            THE GAME HAS ENDED
         </div>
     </main>
    ) 
