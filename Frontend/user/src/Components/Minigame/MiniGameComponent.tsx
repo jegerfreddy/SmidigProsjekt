064025b6a47ee21f4a347b4be17d451ae6411a80
Malin Akre <malin.akre01@gmail.com>
2024-06-05 13:23:23 +0200

    fikset så minigame-alert kommer kun 1 gang!

--- Frontend/user/src/Components/Minigame/MiniGameComponent.tsx
+++ Frontend/user/src/Components/Minigame/MiniGameComponent.tsx
@@ -1,26 +1,28 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 
 const MiniGameBaloons: React.FC = () => {
     const [redValue, setRedValue] = useState(0);
     const [purpleValue, setPurpleValue] = useState(0);
 
     const handleClick = () => {
-        setRedValue(redValue + Math.random() * 10);
-        setPurpleValue(purpleValue + Math.random() * 10);
+        const newRedValue = redValue + Math.random() * 10;
+        const newPurpleValue = purpleValue + Math.random() * 10;
+        setRedValue(newRedValue);
+        setPurpleValue(newPurpleValue);
         //sender value til backend
     }
 
-    //funskjon som heter value fra backend som oppdaterer value¨
-    if(redValue > 100) {
-        setRedValue(0);
-        setPurpleValue(0);
-        alert('Gratulerer! Rød har vunnet spillet')
-    }
-    if(purpleValue > 100) {
-        setRedValue(0);
-        setPurpleValue(0);
-        alert('Gratulerer! lilla har vunnet spillet')
-    }
+    useEffect(() => {
+        if (redValue > 100) {
+            setRedValue(0);
+            setPurpleValue(0);
+            alert('Gratulerer! Rød har vunnet spillet');
+        } else if (purpleValue > 100) {
+            setRedValue(0);
+            setPurpleValue(0);
+            alert('Gratulerer! Lilla har vunnet spillet');
+        }
+    }, [redValue, purpleValue]);
 
     return (
         <>
@@ -28,8 +30,8 @@ const MiniGameBaloons: React.FC = () => {
                 <h1 className="text-center position relative top-0 start-0 p-5">
                     Hjelp laget ditt ved å gi luft til ballongen
                 </h1>
-                <img src="/images/redBaloon.png" alt="redBaloon" className='position-absolute start-0'  style={{ bottom: `${redValue}vh`, transition: 'bottom 0.5s ease' }}/>
-                <img src="/images/purpleBaloon.png" alt="purpleBaloon" className='position-absolute end-0'  style={{ bottom: `${purpleValue}vh`, transition: 'bottom 0.5s ease' }}/>
+                <img src="/images/redBaloon.png" alt="redBaloon" className='position-absolute start-0' style={{ bottom: `${redValue}vh`, transition: 'bottom 0.5s ease' }} />
+                <img src="/images/purpleBaloon.png" alt="purpleBaloon" className='position-absolute end-0' style={{ bottom: `${purpleValue}vh`, transition: 'bottom 0.5s ease' }} />
             </div>
         </>
     );
