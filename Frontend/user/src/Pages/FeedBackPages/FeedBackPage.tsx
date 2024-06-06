76ed9ef5d310892f81eb1573dc941b2066c05a44
parmdeepsinghh <parmdeep.singhh@gmail.com>
2024-06-06 14:53:53 +0200

    feeback endring
    Endret på knappen ti l feedback

--- Frontend/user/src/Pages/FeedBackPages/FeedBackPage.tsx
+++ Frontend/user/src/Pages/FeedBackPages/FeedBackPage.tsx
@@ -1,50 +1,47 @@
-import  {useNavigate} from "react-router-dom"; 
+import { useNavigate } from "react-router-dom"; 
 import { useState } from "react";
 import FeedBackList from "../../Components/FeedBack/FeedBackList";
 import { IFeedBack } from "../../Interfaces/IFeedBack";
 import { FeedBackService } from "../../Services/GetService";
 
-
 const FeedBackPage: React.FC = () => {
     const [actEventId] = useState<string>('');
     const [rating, setRating] = useState<number>(0); 
-    const [userID, setUserId] = useState<number>(1); 
-    const [actID, setActId] = useState<number>(1); 
+    const [userID] = useState<number>(1); 
+    const [actID] = useState<number>(1); 
     const navigate = useNavigate();
 
-
-        const handleClick = (value: number) => {
+    const handleClick = (value: number) => {
         setRating(value);
-        setUserId(1);
-        setActId(1);
         console.log(`Rating: ${value}`, `User ID: ${userID}`, `Act ID: ${actID}`);
     };
 
     const handleSubmit = async (newFeedback: IFeedBack) => {
         try {
-            const result: any = await FeedBackService.post(newFeedback);
-            const postResult = result;
-            console.log(`I page`, postResult);
+            const result = await FeedBackService.post(newFeedback);
+            console.log(`In page`, result);
             navigate("/endgame");
         } catch (error) {
             console.error('Error occurred while submitting user data:', error);
         }
     };
 
-     
-    
     return (
         <main className='feedback-page vh-100 d-flex overflow-hidden align-items-center justify-content-center bgColor'>
             <div className='feedback-content text-center'>
                 <FeedBackList actEventId={actEventId} onClick={handleClick} orientation="horizontal" />
                 <section className="mt-4">
-                    <button className="btn btn-primary btn-lg" onClick={() => handleSubmit({ userID, rating, actID })}>
+                    <button 
+                        className={`btn btn-primary btn-lg ${rating === 0 ? 'disabled' : ''}`}
+                        onClick={() => handleSubmit({ userID, rating, actID })}
+                        disabled={rating === 0} // Knappen er "grå" før stjerne velges.
+                    >
                         Submit Feedback
                     </button>
                 </section>
             </div>
         </main>
     );
-}
+};
 
 export default FeedBackPage;
