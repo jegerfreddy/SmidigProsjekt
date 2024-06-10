d37483548ac2a258958aa36dcbdc51e08dba3ca2
jegerfreddy <vetle.k@hotmail.no>
2024-06-10 11:36:45 +0200

    Color change on input field

--- Frontend/Admin/pages/SettingsPage.tsx
+++ Frontend/Admin/pages/SettingsPage.tsx
@@ -2,6 +2,23 @@ const SettingsPage = () => {
 
     return (
         <>
+            <div className="d-flex flex-column align-items-center p-5">
+
+                <div className="settings-content">
+                    <h2>Logged in as: <i className="text-primary">admin</i></h2>
+
+                    <div className="d-flex flex-column m-5">
+                        <p>Change Username & Password</p>
+                        <input placeholder="New Username" type="text" />
+                        <input placeholder="New Password" type="text" />
+                    </div>
+
+                    <button>Add New Admin</button>
+
+                    <button className="btn btn-success w-100">Save</button>
+                </div>
+
+            </div>
         </>
     );
 };
